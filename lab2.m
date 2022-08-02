bc9e484df6b1f4c0b075d7e15b6e64fb9046022a
Ismail Adebayo <ismail.adebayo89@gmail.com>
2022-08-02 15:49:23 +1000

    Increased value of samples;Added MA function plot

--- lab2.m
+++ lab2.m
@@ -1,10 +1,10 @@
 clc
 clear all
 
-%number of samples
-n = 0:1:40 
+%number of samples increased to 70 as per specs
+n = 0:1:70; 
 %Representation for a 8-point MA filter
-x = 2 * sin(((pi*n)/10)-(pi/3))
+x = 2 * sin(((pi*n)/10)-(pi/3));
 B = 1/8 * ones(8,1);
 output  = filter(B,1,x);
 %plot of the input to the filter
@@ -12,6 +12,11 @@ stem(n,x)
 title('Input');
 xlabel('n')
 ylabel('x(n)')
+%plot of the filter function
+stem(B)
+title('Moving AVerage Filter');
+xlabel('n')
+ ylabel('B')
 %plot of ouput of the filter
 stem(n,output)
 title('Output');
