f1c3143307a26634946d8808dff50d14e477fa35
Ismail Adebayo <ismail.adebayo89@gmail.com>
2022-08-02 14:54:13 +1000

    Add Question 1A

--- lab2.m
+++ lab2.m
@@ -1,8 +1,19 @@
-% Matlab script for lab 2
-% Ismail Adebayo, Johanne Montaono
+clc
+clear all
 
-% test
-x = [0:0.1:10]
-y = x
-
-plot(x, y)
+%number of samples
+n = 0:1:40 
+%Representation for a 8-point MA filter
+x = 2 * sin(((pi*n)/10)-(pi/3))
+B = 1/8 * ones(8,1);
+output  = filter(B,1,x);
+%plot of the input to the filter
+stem(n,x)
+title('Input');
+xlabel('n')
+ylabel('x(n)')
+%plot of ouput of the filter
+stem(n,output)
+title('Output');
+xlabel('n')
+ylabel('y(n)')
